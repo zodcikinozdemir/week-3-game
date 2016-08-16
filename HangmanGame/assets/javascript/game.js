2e40f1cb170795f23a3b868708ec611fbea0a043
zodcikin <zeynep.odcikin@gmail.com>
2016-08-16 00:18:15 -0400

    Hangman Game clean up on the code

--- HangmanGame/assets/javascript/game.js
+++ HangmanGame/assets/javascript/game.js
@@ -12,8 +12,8 @@ String.prototype.replaceAt=function(index, char) {
 }
 
 var letters = [
-               "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-               "p","q","r","s","t","u","v","w","x","y","z"
+				"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+				"p","q","r","s","t","u","v","w","x","y","z"
               ];
 
 
@@ -37,9 +37,9 @@ var hangmanGame = {
 	guessesLeft : 12, 
 	// Your Guesses so far: # of times the user has failed to guess the letter correctly after exhausting all guesses
 	guesses : [], 
-    movie : null,
-    name :"",
-    guessedCorrect:0,
+	movie : null,
+	name :"",
+	guessedCorrect:0,
 
     starANewGame : function () {
     	gameStarted = true;
@@ -49,13 +49,18 @@ var hangmanGame = {
    		this.guesses = [];
    		this.guessedCorrect = 0;
    		this.name = new Array(this.movie.name.length + 1).join( '-' );
-   		document.querySelector('#wins').innerHTML = this.wins;
-    	document.querySelector('#losses').innerHTML = this.losses;
-    	document.querySelector('#guessesLeft').innerHTML = this.guessesLeft;
-    	document.querySelector('#lettersGuessed').innerHTML = hangmanGame.guesses;
-    	document.querySelector('#currentMovie').innerHTML = this.name;
-    	document.querySelector('#picture').src = "assets/images/background.gif";
+		document.querySelector('#wins').innerHTML = this.wins;
+		document.querySelector('#losses').innerHTML = this.losses;
+		document.querySelector('#guessesLeft').innerHTML = this.guessesLeft;
+		document.querySelector('#lettersGuessed').innerHTML = hangmanGame.guesses;
+		document.querySelector('#currentMovie').innerHTML = this.name;
+		document.querySelector('#picture').src = "assets/images/background.gif";
     },
+    
+    checkNameContainsLetter(userLetter) {
+    	return (this.movie.name.includes(userLetter))? true: false;
+    },
+
     rightGuess : function(userLetter) {
     	var movieName = this.movie.name;
     	for (var i = 0; i< movieName.length; i++) {
@@ -72,8 +77,9 @@ var hangmanGame = {
 			document.querySelector('#wins').innerHTML = this.wins;
     	}
     },
+    
     wrongGuess : function(userLetter) {
-    	this.guesses.push(userLetter);
+		this.guesses.push(userLetter);
 		this.guessesLeft--;
 		document.querySelector('#guessesLeft').innerHTML = this.guessesLeft;
 		document.querySelector('#lettersGuessed').innerHTML = this.guesses;
@@ -82,18 +88,15 @@ var hangmanGame = {
 			document.querySelector('#picture').src = this.movie.image;
 			this.losses++
 			document.querySelector('#wins').innerHTML = this.wins;
-    		document.querySelector('#losses').innerHTML = this.losses;
+			document.querySelector('#losses').innerHTML = this.losses;
 		}
-    },
-    checkNameContainsLetter(userLetter) {
-    	return (this.movie.name.includes(userLetter))? true: false;
     }
 }
 
 // When the user presses the key it records the keypress
 document.onkeyup = function(event) {
 	var userLetter = String.fromCharCode(event.keyCode).toLowerCase();
-    if(letters.contains(userLetter) == true) {
+	if(letters.contains(userLetter) == true) {
 		if(gameStarted == false) {
 			hangmanGame.starANewGame();
 		} else {
